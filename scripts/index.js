42075b3fad4e5a0158616922aa18d32a7e18b18a
SJB <s.bangera@castsoftware.com>
2024-11-20 19:28:51 +0530

    Setting up

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+//constants
+const titleKeyword = document.getElementById('animated-keyword');
+
+function animateKeyWords() {
+    const keywords = ['a Champion', 'a Fighter', 'a Survivor', 'the Best'];
+    let index = 0;
+
+    return setInterval(() => {
+        titleKeyword.innerText = keywords[index++];
+        
+        if (index >= keywords.length) {
+            index = 0;
+        }
+    }, 1000);
+}
+
+window.addEventListener('load', e=>{
+    animateKeyWords();
+});
